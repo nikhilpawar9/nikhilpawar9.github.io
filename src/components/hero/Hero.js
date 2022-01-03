2788bb05212c9c0121c9f05a6799ba61b7dbf7b5
Nikhil <nikhilpawar982001@gmail.com>
2022-01-03 23:20:28 +0530

    model positioning done

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,22 +1,35 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Html, OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import Hello from "../Hello";
+import './Hero.css'
+
 const Hero = () => {
-    return (
+  return (
+    <div className="hero">
       <div className="canvas">
-        <Canvas colorManagement camera={{ position: [-5, 0, 10], fov: 60 }}>
-          <OrbitControls />
+        <Canvas
+          colorManagement
+          camera={{
+            position: [-2, 0, 15],
+            fov: 30,
+            zoom: 2,
+            near: 1,
+            far: 1000,
+          }}
+        >
+          <OrbitControls  />
 
           <directionalLight intensity={0.5} />
           <pointLight position={[-10, 0, -20]} intensity={0.4} />
           <ambientLight intensity={0.3} />
-          <Suspense fallback={null}>
+          <Suspense fallback={<Html>Loading...</Html>}>
             <Hello />
           </Suspense>
         </Canvas>
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
